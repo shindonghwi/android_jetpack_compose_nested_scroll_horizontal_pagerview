987245bc195a7cd38ee7e66c97365f9854ea9d72
ShinDongHwi <ehdgnl8940@naver.com>
2022-03-31 01:19:44 +0900

    feat: NestedScrollview 샘플 기능 구현

--- app/src/main/java/com/example/jetpackcompose_netstedscroll_horizontalpagerview/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose_netstedscroll_horizontalpagerview/MainActivity.kt
@@ -3,36 +3,131 @@ package com.example.jetpackcompose_netstedscroll_horizontalpagerview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose_netstedscroll_horizontalpagerview.ui.theme.JetpackCompose_NetstedScroll_HorizontalPagerViewTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackCompose_NetstedScroll_HorizontalPagerViewTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = Color.Black
+                ) {
+                    NestedScrollview()
                 }
+
             }
         }
     }
 }
 
+@OptIn(ExperimentalPagerApi::class, ExperimentalMaterialApi::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackCompose_NetstedScroll_HorizontalPagerViewTheme {
-        Greeting("Android")
+fun NestedScrollview() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "VerticalNestedScrollView",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                },
+                elevation = 0.dp
+            )
+        }
+    ) {
+        val scope = rememberCoroutineScope()
+        val nestedScrollViewState = rememberNestedScrollViewState()
+        VerticalNestedScrollView(
+            state = nestedScrollViewState,
+            header = {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    color = Color.Yellow,
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(20.dp)
+                    ) {
+                        Text(text = "Make it Easy set of Header")
+                        Text(text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+                    }
+                }
+            },
+            content = {
+                val pagerState = rememberPagerState(initialPage = 0)
+                val pages = (0 until 4).map { it }
+                Column {
+                    TabRow(
+                        selectedTabIndex = pagerState.currentPage,
+                        indicator = { tabPositions ->
+                            TabRowDefaults.Indicator(
+                                Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+                            )
+                        }
+                    ) {
+                        pages.forEachIndexed { index, title ->
+                            Tab(
+                                text = { Text(text = "Tab ${title + 1}") },
+                                selected = pagerState.currentPage == index,
+                                onClick = {
+                                    scope.launch {
+                                        pagerState.animateScrollToPage(index)
+                                    }
+                                },
+                            )
+                        }
+                    }
+                    HorizontalPager(
+                        modifier = Modifier.weight(1f),
+                        state = pagerState,
+                        count = 4,
+                    ) {
+                        LazyColumn {
+                            items(50) {
+                                ListItem {
+                                    Card(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(5.dp),
+                                        shape = RoundedCornerShape(5.dp),
+                                        elevation = 5.dp
+                                    ) {
+                                        Text(
+                                            text = "Jetpack Compose ${it + 1}",
+                                            modifier = Modifier.padding(15.dp)
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        )
     }
 }
+
+
+
+
+
